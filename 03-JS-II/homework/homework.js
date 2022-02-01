5569515f8e7d2616559582430dfdb5343c25ba14
rodber912 <rodrigo.bernad@gmail.com>
2022-02-01 16:48:41 -0300

    Prueba hw2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -11,6 +11,8 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if edad > 18 {return 'Allowed'}
+  return 'Not allowed';
 }
   
 function conection(status) {
