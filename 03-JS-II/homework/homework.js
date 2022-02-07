c65a6800e4a4e567c3ce06ee3f137fd967db4497
rodber912 <rodrigo.bernad@gmail.com>
2022-02-07 19:51:04 -0300

    Prueba 99

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -73,6 +73,10 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || 5) {
+    return true
+  }
+  else false
 }
 
 function estaEnRango(numero) {
