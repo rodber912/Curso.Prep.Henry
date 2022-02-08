d6509007610b924ebbe13837491dfcae170bc080
rodber912 <rodrigo.bernad@gmail.com>
2022-02-08 12:16:40 -0300

    Prueba1234

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -103,7 +103,7 @@ function fizzBuzz(numero) {
   // De lo contrario, devuelve el numero
   if (numero % 3 === 0) return 'fizz';
   if (numero % 5 === 0) return 'buzz';
-  if (numero % 15 === 0) return 'fizzbuzz';
+  if (numero % 3 === 0 && numero % 5 === 0) return 'fizzbuzz';
   return numero;
 }
 
