674869190e358847596acf3d88c2786d7b717941
rodber912 <rodrigo.bernad@gmail.com>
2022-02-14 11:45:11 -0300

    v14022

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -102,8 +102,8 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
   if (numero % 3 === 0) return 'fizz';
-  if (numero % 5 === 0) return 'buzz';
-  if (numero % 15 === 0) return 'fizzbuzz';
+  else if (numero % 5 === 0) return 'buzz';
+  if (numero % 3 === 0 && numero % 5 === 0) return 'fizzbuzz';
   return numero;
 }
 
