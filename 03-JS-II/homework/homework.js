418335c7a2e72fe12ef2ee6d31ae06570d8bc3ca
rodber912 <rodrigo.bernad@gmail.com>
2022-02-14 13:00:18 -0300

    v140222.1

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -101,12 +101,11 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if (numero % 3 === 0) return 'fizz';
-  else if (numero % 5 === 0) return 'buzz';
-  if (numero % 15 === 0) return 'fizzbuzz';
+  if(numero % 15 === 0) return 'fizzbuzz';
+  if(numero % 3 === 0) return 'fizz';
+  if(numero % 5 === 0) return 'buzz';
   return numero;
 }
-
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
   //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
@@ -114,12 +113,13 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
-  if (num1 > 0 && num1 > num2 && num1 > num3) {
-    return 'Número 1 es mayor y positivo';
-  }
-  else if (num1 < 0 || num2 < 0 || num3 < 0) {
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
     return 'Hay negativos';
   }
+  else if (num1 > 0 && num1 > num2 && num1 > num3) {
+    return 'Número 1 es mayor y positivo';
+  }
+  
   else if (num1 === 0 || num2 === 0 || num3 === 0) {
     return 'Error';
   }
