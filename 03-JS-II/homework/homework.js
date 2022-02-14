a7c63d1e08f834e7f95e3f9567a596c228d9c25e
rodber912 <rodrigo.bernad@gmail.com>
2022-02-14 17:56:52 -0300

    v140222.8

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -158,21 +158,33 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí  
-  for (let i = 6 ; i === 60 ; i *= 1++) {
-    return i
-  }
+  let tabla6 = []
+  for (i = 0 ; i < 11 ; i++)
+  tabla6.push (6 * i)
+
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero.lenght > 99 && numero < 1000) { 
+    return true
+  }
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var a = numero
+  var i = 0
+  do {
+    i = i + 1;
+    a = a + 5;
+  }
+  while (i < 8);
+  return a;
+  }
 }
 
 
