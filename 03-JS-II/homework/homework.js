0b690a646693077a451aad1cb6c38ff137dbf340
rodber912 <rodrigo.bernad@gmail.com>
2022-02-14 17:15:43 -0300

    V140222.3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -148,12 +148,10 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if (valor === x && x === x) {
+  if (valor === true) {
     return 'Soy verdadero'
-  }
-  else if (valor === x && x < x) {
+  }  
     return 'Soy falso'
-  }
 }
 
 function tablaDelSeis(){
